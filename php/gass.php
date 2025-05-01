e2f3170373119d269b17d22e9a03502625dd5d06
bahriyanto31 <bahriyantoanang@gmail.com>
2025-05-01 19:35:40 +0800

    commit

--- php/gass.php
+++ php/gass.php
@@ -1,6 +1,8 @@
 <?php
 for ($i = 1; $i <= 365; $i++) {
     $commitCount = rand(1, 10);
+    echo "Hari ke-$i: $commitCount commit\n";
+
     for ($j = 0; $j < $commitCount; $j++) {
         $d = $i . " days ago";
 
@@ -10,8 +12,12 @@ for ($i = 1; $i <= 365; $i++) {
         // Jalankan perintah Git
         exec('git add .');
         exec('git commit --date="' . $d . '" -m "commit"');
+
+        echo "  Commit ke-" . ($j + 1) . " dengan tanggal $d\n";
     }
 }
 
 // Push ke origin
+echo "Push ke origin...\n";
 exec('git push -u origin main');
+echo "Selesai!\n";
